d04f8d8b11dc0f9c72d65d19f8b1fc307113b596
Evandro Santos <rm84235@fiap.com.br>
2020-12-17 02:13:27 -0300

    Version update

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hkf16yj5b79k'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    """
-
-    :return:
-    """
-    return 'Hello World!'
-
+import routes
 
 if __name__ == '__main__':
-    app.run()
+    routes.app.run(debug=True)
